66616d5e59a9970c2f362669862acd31068cfb13
Artem Sharov <sharov.a.p.10@gmail.com>
2020-04-06 22:32:20 +0300

    task_1 .

--- SQL_Query.java
+++ SQL_Query.java
@@ -1,8 +1,18 @@
 package ru.gb.java_3.hw_2;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+1. Сделать методы для работы с БД (CREATE, UPDATE, DELETE, INSERT, SELECT)
+ */
+
 public class SQL_Query {
         private static Connection connection;
         private static Statement statement;
+        private static PreparedStatement pstmt;
+        public static Scanner sc = new Scanner(System.in);
+
 
         synchronized static void connect() {
             try {
@@ -21,6 +31,54 @@ public class SQL_Query {
                 throw new RuntimeException(e);
             }
         }
+
+        public static void createTable(){
+            try {
+                Statement stmt = connection.createStatement();
+                boolean rs = stmt.execute("CREATE TABLE IF NOT EXISTS Students" +
+                        "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                        "  Name TEXT NOT NULL," +
+                        "  Score INTEGER NOT NULL);");
+                           } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        public static void update(String columnName, int param){
+            try {
+                Statement create = connection.createStatement();
+                pstmt = connection.prepareStatement("UPDATE Students SET Score = ? WHERE Name = ?;");
+                pstmt.setInt(1, param);
+                pstmt.setString(2, columnName);
+                ResultSet rs = pstmt.executeQuery();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    public static void insert(int id, String name, int score){
+        try {
+            Statement create = connection.createStatement();
+            pstmt = connection.prepareStatement("INSERT INTO Students (ID, Name, Score) VALUES (?, ?, ?);");
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            pstmt.setInt(3, id);
+            pstmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        connect();
+
+        createTable();
+        insert(1,"Bob",30);
+
+        disconnect();
+    }
     }
 
 
--- UpdateScoreStudent.java
+++ UpdateScoreStudent.java
@@ -1,5 +1,5 @@
 package ru.gb.java_3.hw_2;
 
 public class UpdateScoreStudent {
-    
+
 }
